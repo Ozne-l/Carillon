3587ae6942ce7a8b75f30d25d0a0064ae009de3b
Enzo Labat <elabat@localhost.localdomain>
2019-12-12 14:17:50 +0100

    Front [rework] update UI & UX

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:dio/dio.dart';
 import 'package:device_info/device_info.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 
 var serverIp = "192.168.1.2";
@@ -47,6 +48,10 @@ class _MyHomePageState extends State<MyHomePage> {
     bytes = null;
   }
 
+  void openGallery() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    postImage(image, false);
+  }
 
   void openCamera() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
@@ -78,34 +83,103 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            display == true ?
-            Container(
-              height: 515,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image : MemoryImage(bytes),
-                  fit: BoxFit.cover
-                )
-              ),
-            ) :
-            Text('Take a picture')
-            // res == true ? Image.memory(bytes) : Text('Take a picture')
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+            Color(0xFF1b1e44),
+            Color(0xFF2d3447),
           ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            )
+          ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 2,
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          title: Text(widget.title, style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontFamily: "Calibre-Semibold",
+                  letterSpacing: 1.0)),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: openCamera,
-        tooltip: 'Take picture',
-        child: Icon(Icons.camera),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Card(
+            color: Colors.transparent,
+            elevation: 0,
+            margin: const EdgeInsets.only(
+              bottom: 50,
+              left: 10,
+              right: 10
+            ),
+            child: display == true ?
+                Container(
+                  height: 515,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(const Radius.circular(5)),
+                    image: DecorationImage(
+                      image : MemoryImage(bytes),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                ) :
+                Text('Take or import a picture', style: TextStyle(color: Colors.white, fontSize: 20, fontFamily: "Calibre-Semibold", letterSpacing: 1.0))
+            // Column(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: <Widget>[
+            //     display == true ?
+            //     Container(
+            //       height: 515,
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.only(
+            //             topLeft: const Radius.circular(5),
+            //             topRight: const Radius.circular(5)
+            //         ),
+            //         image: DecorationImage(
+            //           image : MemoryImage(bytes),
+            //           fit: BoxFit.cover
+            //         )
+            //       ),
+            //     ) :
+            //     Text('Take or import a picture', style: TextStyle(color: Colors.white, fontSize: 20, fontFamily: "Calibre-Semibold", letterSpacing: 1.0))
+            //     // res == true ? Image.memory(bytes) : Text('Take a picture')
+            //   ],
+            // ),
+          ),
+        ),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_arrow,
+          animatedIconTheme: IconThemeData(size: 22),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          visible: true,
+          curve: Curves.bounceIn,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.photo_album, color: Color(0xFF1b1e44)),
+              backgroundColor: Colors.white,
+              onTap: openGallery,
+              label: 'Gallery',
+              labelStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0),
+              labelBackgroundColor: Color(0xFF1b1e44)),
+            SpeedDialChild(
+              child: Icon(Icons.add_a_photo, color: Color(0xFF1b1e44)),
+              backgroundColor: Colors.white,
+              onTap: openCamera,
+              label: 'Camera',
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+                fontSize: 16.0),
+              labelBackgroundColor: Color(0xFF1b1e44))
+          ],
+        )
+      )
     );
   }
 }
