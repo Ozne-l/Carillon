371abb32c2710064011d7924e801b7fc5c78674e
Léo HUIN <OxeLine@ip-72.net-89-3-40.rev.numericable.fr>
2020-02-10 19:03:54 +0100

    begin of the landing page : Flex display + form for email

--- com/landing_page/app.py
+++ com/landing_page/app.py
@@ -9,9 +9,13 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        print (request.form["email"])
+        return render_template("index_thanks.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
